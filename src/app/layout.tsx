73a1108c5a9bcdaebafab395a03c733c0c138140
Elif Nur Aslıhan Celepoğlu <84383557+elif1906@users.noreply.github.com>
2024-05-15 01:53:45 +0300

    change design home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Link from "next/link";
 import Image from "next/image";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,15 +20,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        
         <div className="flex gap-8 justify-between w-screen bg-white">
-            <div className="pl-8 py-2">
-              <Image width={100} height={100} src="/pycimen-logo.jpeg" alt="PyCimen"></Image>
+            <div className="pl-12 py-2">
+              <Image width={80} height={80} src="/pycimen-logo.png" alt="PyCimen"></Image>
             </div>
-            <div className="flex gap-20 justify-end pt-8 pr-12">
-              <Link className="text-black text-xl" href="/">Home</Link>
-              <Link className="text-black text-xl" target="_blank" href="/docs.pdf">Docs</Link>
-              <Link className="text-black text-xl" target="_blank" href="https://github.com/elif1906/Pycimen/releases/tag/v1.0.0">Installation</Link>
-              <Link className="text-black text-xl" href="https://github.com/elif1906/pycimen.git"><Image width={30} height={30} src="/github.jpeg" alt="PyCimen"></Image></Link>
+            <div className="flex gap-20 justify-end pt-10 pr-14 ">
+              <Link style={{ height: "fit-content" }} className="text-black text-xl hover:border-b-2 hover:border-green-700" href="/">Home</Link>
+              <Link style={{ height: "fit-content" }} className="text-black text-xl hover:border-b-2 hover:border-green-700" target="_blank" href="/docs.pdf">Docs</Link>
+              <Link style={{ height: "fit-content" }} className="text-black text-xl hover:border-b-2 hover:border-green-700" target="_blank" href="https://github.com/elif1906/Pycimen/releases/tag/v1.0.0">Installation</Link>
+              <Link style={{ height: "fit-content" }} className="text-black text-xl hover:border-b-2 hover:border-green-700 " target="_blank" href="https://github.com/elif1906/pycimen.git"><Image width={30} height={30} src="/github.jpeg" alt="PyCimen"></Image></Link>
+              <Link style={{ height: "fit-content" }} className="text-black text-xl hover:border-b-2 hover:border-green-700 " target="_blank" href="https://www.linkedin.com/in/elif-nur-aslıhan-celepoğlu/"><Image width={30} height={30} src="/linkedin.png" alt="PyCimen"></Image></Link>
+
             </div>
             
         </div>
